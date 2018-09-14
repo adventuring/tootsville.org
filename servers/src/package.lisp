30d3eef88efc1ec2c83fe1892ce4b01156132035
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2018-09-13 23:07:04 -0400

    thoroughly pollute the namespace

--- servers/src/package.lisp
+++ servers/src/package.lisp
@@ -2,8 +2,9 @@
 (in-package :cl-user)
 (restas:define-module tootsville
   (:documentation  "Let's make some noise!")
-  (:use :alexandria :cl :local-time :org.star-hope.utils)
-  (:import-from :split-sequence :split-sequence)
+  (:use #:alexandria #:cl #:local-time #:org.star-hope.utils #:oliphaunt)
+  (:shadowing-import-from #:cl-fad #:copy-file #:copy-stream) ; conflicts with Alexandria.
+  (:import-from #:split-sequence #:split-sequence)
   (:import-from #:envy
                 #:config-env-var
                 #:defconfig)
