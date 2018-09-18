e6578b092a937bc8b0bfde45b90d1440ad366976
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2018-09-18 08:53:53 +0000

    async tasks thread pool

--- servers/src/main.lisp
+++ servers/src/main.lisp
@@ -91,6 +91,43 @@ redirect to default host"
                   (hunchentoot:acceptor-port acceptor)))
       (return-from find-acceptor acceptor))))
 
+(defvar *async-tasks* nil)
+
+(defconstant +async-worker-threads+ 2)
+
+(defun name-all-async-threads-idle ()
+  (loop for thread in (slot-value *async-tasks*
+                       'cl-threadpool::threads)
+     for i fixnum from 1
+     with count = (taskmaster-max-thread-count taskmaster)
+     do (setf (sb-thread:thread-name thread)
+              (format nil "Idle Asyncronous Worker (#~d of ~d)" i count))))
+
+(defun init-async ()
+    (setf *async-tasks*
+        (cl-threadpool:make-threadpool
+          +async-worker-threads+
+          :max-queue-size 1024
+          :name "Asynchronous Workers"
+          :resignal-job-conditions (not (swank-connected-p))))
+  (cl-threadpool:start *async-tasks*)
+  (name-all-threads-idle *async-tasks*))
+
+(defun run-async (function)
+  (unless *async-tasks*
+     (init-async))
+  (cl-threadpool:add-job *async-tasks*
+    (lambda ()
+       (let ((idle-name (thread-name (current-thread))))
+         (setf (sb-thread:thread-name (current-thread)) (format nil "Async: run ~s" function))
+         (unwind-protect
+              (thread-pool-taskmaster::with-pool-thread-restarts ((thread-name (current-thread)))
+                (verbose:info '(:threadpool-worker :async-worker :worker-start) "{~a}: working" (thread-name (current-thread)))
+                (funcall function))
+           (verbose:info '(:threadpool-worker :async-worker :worker-finish) "{~a}: done" (thread-name (current-thread)))
+           (setf (sb-thread:thread-name (current-thread)) idle-name))))))
+
+
 (defun start (&key (host "localhost") (port 5000))
   "Start a local Hunchentoot server.
 
