81363f778485d6a933a97e6689ba89b118ae55d0
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2018-09-18 22:17:05 +0000

    infrastructure for POST

--- servers/src/power-on-self-test.lisp
+++ servers/src/power-on-self-test.lisp
@@ -1,5 +1,17 @@
 (in-package :tootsville)
 
+(defvar *post-tests-queue*)
+
+(defmacro defpost (name (&key) &body body)
+  "Define  a power-on-self-test  from  somewhere else  in the  codebase.
+  These are run as confidence tests  after a build, or during Production
+  boot-up sequence."
+  (let ((fn-name (intern (concatenate 'string "⊕POST-" name))))
+    `(progn
+       (defun ,fn-name () ,@body)
+       (pushnew ',fn-name *post-tests-queue*))))
+
+
 (defun post/read-version-page (port)
   "Power-On-Self-Test:  Checks  that  the  server  can  respond  to  the
 version-page query locally."
@@ -20,6 +32,26 @@ a moment and try that again.~%" c)
                     (sleep 1)
                     (go retry-post))))))))
 
+(defpost post-version-check ()
+  (let ((port (+ (random 10) 27700)))
+    (unwind-protect 
+         (progn (handler-case (start :port port)
+                  (simple-error (c) (if (find-restart :restart-server)
+                                        (invoke-restart :restart-server)
+                                        (signal c))))
+                (sleep 1/2)        ; start time
+;;; something that appears on the version page, but no error pages.
+                (let ((reply (prog1 (post/read-version-page port)
+                               (stop))))
+                  (unless (search "Bruce-Robert Pocock" reply)
+                    (warn "Failed POST~%got~%~a" reply)
+                    (if exitp
+                        (cl-user::exit :code 27 :abort t :timeout 5)
+                        (return-from power-on-self-test nil))
+                    nil)))
+      (stop :port port))))
+
+
 (defun power-on-self-test (&key (exitp t))
   "Perform some sanity checking as a part of testing.
 
@@ -28,24 +60,20 @@ need to be expanded a great deal to increase confidence in these tests."
   (fresh-line)
   (princ "Power-on self-test:")
   (fresh-line)
-  (let ((port (+ (random 10) 27700)))
-    (handler-case (start :port port)
-      (simple-error (c) (if (find-restart :restart-server)
-                            (invoke-restart :restart-server)
-                            (signal c))))
-    (sleep 1/2) ; start time
- ;;; something that appears on the version page, but no error pages.
-    (let ((reply (prog1 (post/read-version-page port)
-                   (stop))))
-      (unless (search "Bruce-Robert Pocock" reply)
-        (warn "Failed POST~%got~%~a" reply)
-        (if exitp
-            (cl-user::exit :code 27 :abort t :timeout 5)
-            (return-from power-on-self-test nil))
-        nil)))
-  (fresh-line)
-  (princ "Passed POST")
-  (fresh-line)
-  (stop)
-  t)
+  (let ((warnings 0) (errors 0))
+    (dolist (test *post-tests-queue*)
+      (handler-case
+          (funcall test)
+        (warning (incf warnings))
+        (error (incf errors))))
+    (format t "~&Power-On Self Test: ~:[no errors~; ~r error~:p~], and ~:[no warnings~;~r warning~:p~].~&"
+            errors warnings)
+    (cond ((or (and (productionp) (plusp errors))
+               (> errors 4)
+               (> warnings 5))
+           (princ "POST Failed")
+           nil)
+          (t (princ "POST Passed")
+             t))))
+
 
