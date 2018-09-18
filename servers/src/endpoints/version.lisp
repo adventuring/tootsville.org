b51f6477f36379ef45c89205fcddc79e5ad5b590
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2018-09-18 22:05:55 +0000

    Lower /version to /version/about
    Makes life better for the load balancers and things

--- servers/src/endpoints/version.lisp
+++ servers/src/endpoints/version.lisp
@@ -1,18 +1,18 @@
 ;;;; endpoints /version/*
 (in-package :Tootsville)
 
-(defendpoint (:get "/version" "application/json")
+(defendpoint (:get "/version/about" "application/json")
   (list 200 nil (plist-alist (version-info-list))))
 
-(defendpoint (:get "/version" "text/plain")
+(defendpoint (:get "/version/about" "text/plain")
   (list 200 nil (version-info-report-string '(:*))))
 
-(defendpoint (:get "/version/:param" "text/plain")
+(defendpoint (:get "/version/about/:param" "text/plain")
   (list 200 nil
         (version-info-report-string
          (uiop:split-string param :separator "/"))))
 
-(defendpoint (:get "/version/:param" "application/json")
+(defendpoint (:get "/version/about/:param" "application/json")
   (list 200 nil
         (list param
               (version-info-report-string
