5c71e167b45f50eccf7b44e59eeaba28ef3e2eac
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2018-09-21 03:02:08 +0000

    add docs, mostly

--- servers/src/errors.lisp
+++ servers/src/errors.lisp
@@ -1,25 +1,38 @@
 (in-package :Tootsville)
 
+
+;;; Convenience functions for examining conditions/errors
+
 (defun condition-name (condition)
+  "Returns the capitalized name of the class of CONDITION."
   (string-capitalize (symbol-name (class-name (class-of condition)))))
 
-(defun condition-slots (condition)
+(defun condition-slots (object)
+  "Enumerates the name of every slot on OBJECT"
   (mapcar #'closer-mop:slot-definition-name
-          (closer-mop:class-slots (class-of condition))))
+          (closer-mop:class-slots (class-of object))))
 
-(defun slot-values (obj)
-  (loop for slot in (condition-slots obj)
+(defun slot-values (object)
+  "For any OBJECT, this returns a list; each element is a PList with a slot
+  name and value, encoded in JSON."
+  (loop for slot in (condition-slots object)
      collecting
        (list :slot (symbol-name slot)
-             :value (jonathan.encode:%to-json (slot-value obj slot)))))
+             :value (jonathan.encode:%to-json (slot-value object slot)))))
+
+
+;;; Backtrace handling
 
 (defparameter +backtrace-regex+ "\\n\\w*\\d+:"
   "A regular expression to split backtraces")
 
 (defun split-backtrace (str)
+  "Split a string backtrace into parts"
   (ppcre:split +backtrace-regex+ str))
 
 (defun parse-backtrace (bt)
+  "Break lines of a backtrace into error messag, date/time, and call frames
+  (stack)" 
   (destructuring-bind (header &rest frames) (split-backtrace bt)
     (let ((error-msg (subseq header
                              (position #\: header :from-end t)))
@@ -28,7 +41,11 @@
                              (position #\A header))))
       (list error-msg date-time frames))))
 
+
+;;; JSON encodings — XXX move these somewhere more reasonable
+
 (defmethod jonathan.encode:%to-json ((pathname pathname))
+  "Encode PATHNAME as a JSON object"
   (jonathan.encode:%to-json
    `(:is-a "pathname"
            :host ,(typecase (pathname-host pathname)
@@ -42,6 +59,8 @@
            :type ,(pathname-type pathname))))
 
 (defmethod jonathan.encode:%to-json ((function function))
+  "Encode FUNCTION as a JSON object."
+  ;; TODO try to extract its source tree and pass it along, as well.
   (let ((name (nth-value 2 (function-lambda-expression #'jonathan.encode:%to-json))))
     (jonathan.encode:%to-json
      `(:is-a "function"
@@ -49,11 +68,17 @@
              :name ,(string-upcase (symbol-name name))))))
 
 (defmethod jonathan.encode:%to-json ((object t))
+  "Return a JSON object that represents the state of OBJECT"
   (jonathan.encode:%to-json
    `(:is-a ,(string-capitalize (type-of object))
            :t ,(format nil "~s" object))))
 
+
+;;; Rendering a backtrace … who defines this generic function? XXX
+;;; XXX how is this not CLIM:PRESENT or so?
+
 (defmethod render (bt condition env)
+  "☠ deprecated"
   (let* ((backtrace (parse-backtrace bt)))
     (encode-json
      `(:error ,(princ-to-string condition)
@@ -66,7 +91,10 @@
               :env ,env
               ))))
 
+;;; XXX make sure we don't use this
+
 (defun present-error-to-client (condition env)
+  "☠ deprecated, do not use"
   (let ((backtrace (with-output-to-string (stream)
                      (write-string
                       (print-backtrace condition
@@ -78,7 +106,10 @@
                         :condition condition
                         :env env)))))
 
+;;; this might actually be TRTTD with RESTAS — or some variation XXX
+
 (defun middleware (app)
+  "☠ deprecated"
   (lambda (env)
     (tagbody do-over
        (restart-bind
@@ -88,3 +119,6 @@
          (handler-bind
              ((error (lambda (c) (present-error-to-client c env))))
            (funcall app))))))
+
+
+
