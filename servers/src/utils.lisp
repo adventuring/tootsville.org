a88c167460ffbdfb3048794e68b3f024b2b28cd4
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2018-09-21 16:50:41 +0000

    time tools

--- servers/src/utils.lisp
+++ servers/src/utils.lisp
@@ -58,6 +58,40 @@
 
 
 
+;;; Time handling.
+
+(defun days-ago (days)
+  "Return a time that is DAYS days in the past."
+  (local-time:timestamp- (local-time:now) days :day))
+
+(defun yesterday ()
+  "Get the same time as now, but yesterday.
+
+See: `DAYS-AGO'"
+  (days-ago 1))
+
+(defun 2-days-ago ()
+  "Two days ago.
+
+See: `DAYS-AGO'"
+  (days-ago 2))
+
+(defun 3-days-ago ()
+  "Three days ago.
+
+See: `DAYS-AGO'"
+  (days-ago 3))
+
+(defun header-time (&optional (time (get-universal-time)))
+  "Format TIME (or now) as an RFC-1123 timestring for HTTP headers.
+
+Accepts either a LOCAL-TIME:TIMESTAMP or NUMBER of Universal Time."
+  (local-time:format-rfc1123-timestring
+   nil
+   (etypecase time
+     (number (local-time:universal-to-timestamp time))
+     (local-time:timestamp time))))
+
 (defun year<-universal-time (time)
   (nth-value 5 (decode-universal-time time)))
 
