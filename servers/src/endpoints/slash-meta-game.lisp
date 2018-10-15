9be36552385275568605df983f91cc5112395755
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2018-10-15 19:19:44 -0400

    OpenAPI non-working support; add docstrings to HTML

--- servers/src/endpoints/slash-meta-game.lisp
+++ servers/src/endpoints/slash-meta-game.lisp
@@ -85,7 +85,52 @@
                (decorate-route-template-html (getf route :template)
                                              (getf route :variables)
                                              (getf route :method))
-               "</tt></li>" #(#\Newline)))
+               "</tt> <br>"
+               (docstring->html (getf route :docstring))
+               "</li>" #(#\Newline)))
+
+(defun template->openapi (template)
+  (regex-replace-all "\\:([a-zA-Z0-9-]*)"
+                     template
+                     (lambda (whole _ __ match-start match-end ___ ____)
+                       (declare (ignore _ __ ___ ____))
+                       (concatenate
+                        'string
+                        "{"
+                        (symbol-munger:lisp->camel-case (subseq whole
+                                                                (1+ match-start)
+                                                                match-end))
+                        "}"))))
+
+(defun route->openapi (route)
+  (list (string-downcase (getf route :method))
+        (list :|summary|
+              (getf route :docstring)
+              :|parameters|
+              (route-vars->openapi route)
+              :|responses|
+              (list "200"
+                    (list :|description|
+                          "success (results are not documented in OpenAPI correctly. TODO)")
+                    "default"
+                    (list :|description|
+                          "failure (results are not documented in OpenAPI correctly. TODO)")))))
+
+(defun path->openapi (route-group)
+  (destructuring-bind (path &rest routes) route-group
+    (list (template->openapi path)
+          (loop for route in routes
+             appending
+               (route->openapi route)))))
+
+(defun route-vars->openapi (route)
+  (loop for var in (getf route :variables)
+     collecting
+       (list :|name| (symbol-munger:lisp->camel-case var)
+             :|in| "query"
+             :|description| (string-capitalize var)
+             :|required| t
+             :|schema| (list :|type| "string"))))
 
 (defun routes-prefixed (routes)
   (let ((map (group-by routes :test 'equal
@@ -102,6 +147,14 @@
        do (setf (cdr row) nil))
     map))
 
+(defun group-plists (plists key &key (test 'eql))
+  (let ((hash (make-hash-table :test test)))
+    (dolist (plist plists)
+      (if (gethash (getf plist key) hash nil)
+          (appendf (gethash (getf plist key) hash nil) plist)
+          (setf (gethash (getf plist key) hash nil) (list plist))))
+    (hash-table-alist hash)))
+
 (defendpoint (get "/meta-game/services" "text/html")
   (list 200 ()
         (reduce (curry #'concatenate 'string)
@@ -109,9 +162,23 @@
                                (mapcar (lambda (prefix-group)
                                          (format nil "~%<h5>~a</h5>~{~%~a~}"
                                                  (car prefix-group)
-                                                 (mapcar #'route->html (cdr prefix-group))))
+                                                 (mapcar #'route->html
+                                                         (cdr prefix-group))))
                                        (routes-prefixed (enumerate-routes)))
                                (endpoints-page-footer))))))
 
 (defendpoint (get "/meta-game/services" "application/json")
   (list 200 () (list :services (enumerate-routes))))
+
+(defendpoint (get "/meta-game/services/users"
+                  "application/vnd.oai.openapi;version=3.0")
+  (list 200 () (jonathan:to-json
+                (list :|openapi| "3.0.0" 
+                      :|info| (list :|version| (romance-ii-program-version)
+                                    :|title| (romance-ii-program-name)
+                                    :|license| (list :|name| "AGPLv3"))
+                      :|servers| (list (list :|url| (format nil "https://users.~a.tootsville.org/users/" *cluster*)))
+                      :|paths|
+                      (mapcan #'path->openapi
+                              (group-plists (enumerate-routes) :template))
+                      :|components| #()))))
