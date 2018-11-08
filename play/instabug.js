aefb1240e5b11a279add29ea6966ddb53900979a
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2018-11-08 03:33:17 -0500

    fix label

--- play/instabug.js
+++ play/instabug.js
@@ -1,5 +1,5 @@
 if ('local' == Tootsville.cluster)
-{ console.log("Local: no Rollbar"); }
+{ console.log("Local: no Instabug"); }
 else
 { ibgSdk.init({
     token: 'a1f625a30b0071644267de171fe2bed6'
