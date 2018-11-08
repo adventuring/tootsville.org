d12e3a076d1069671146e09187480a74e65df3f0
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2018-11-07 21:08:18 -0500

    register ServiceWorker

--- play/register-worker.js
+++ play/register-worker.js
@@ -1,18 +1,12 @@
-if (!window['Tootsville']) { window.Tootsville = {} }
-
-Tootsville.registerServiceWorker = function () {
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function() {
-            navigator.serviceWorker.register('/worker.js').then(registration => {
-                Tootsville.inform('ServiceWorker registration successful with scope: ', registration.scope);
-            }, err => {
-                Tootsville.error('ServiceWorker registration failed: ', err);
-            });
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/worker.js').then(registration => {
+            Tootsville.inform('ServiceWorker registration successful with scope: ', registration.scope);
+        }, err => {
+            Tootsville.error('ServiceWorker registration failed: ', err);
         });
-    } else {
-        alert('Can not register ServiceWorker.\nThis is a required feature to play the game.');
-        document.location = 'https://wiki.tootsville.org/wiki/Can_not_register_ServiceWorker';
-    }
+    });
+} else {
+    alert('Can not register ServiceWorker.\nThis is a required feature to play the game.');
+    document.location = 'https://wiki.tootsville.org/wiki/Can_not_register_ServiceWorker';
 }
-
-Tootsville.registerServiceWorker();
