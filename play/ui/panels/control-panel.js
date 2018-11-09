b8fd9f566f3eed0752d15cc887a14f05026c5308
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2018-11-09 16:01:29 -0500

    tweak Control Panel

--- play/ui/panels/control-panel.js
+++ play/ui/panels/control-panel.js
@@ -2,6 +2,7 @@ if (! document.documentElement.requestFullscreen) {
     document.getElementById('control-panel-fullscreen').className = "disabled";
     document.getElementById('fullscreen-toggle').disabled = true;
 }
+
 Tootsville.ui.setFullscreenFromNavigator();
 
 if (navigator.userAgent.indexOf("Firefox/") > 0) {
