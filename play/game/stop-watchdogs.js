dae7bf93cf5ae0ec4e0a14a87b250f405de9c411
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2018-11-23 00:50:16 -0500

    start Render loop

--- play/game/stop-watchdogs.js
+++ play/game/stop-watchdogs.js
@@ -1,18 +1,17 @@
-Tootsville.game.hideWhenGameReady = function () {
-    var toHide = document.querySelectorAll('.hide-when-game-ready');
-    for (var i = 0; i < toHide.length; i++) {
-        toHide[i].style.display = 'none';
-    }
-}
+if (!("game" in Tootsville))
+{ Tootsville.game = {}; }
 
-Tootsville.game.stopSlowLoadingWatchdogs = function () {
-    clearTimeout(window.loadWatchdog1);
+Tootsville.game.hideWhenGameReady = function ()
+{ var toHide = document.querySelectorAll('.hide-when-game-ready');
+  for (var i = 0; i < toHide.length; i++)
+  { toHide[i].style.display = 'none'; }};
+
+Tootsville.game.stopSlowLoadingWatchdogs = function ()
+{ clearTimeout(window.loadWatchdog1);
     clearTimeout(window.loadWatchdog2);
     window.loadWatchdog1 = null;
     window.loadWatchdog2 = null;
 
     var dialog = document.getElementById('elt-Slow-Loading-');
-    if (dialog) {
-        dialog.parentNode.removeChild(dlg);
-    }
-};
+  if (dialog)
+  { dialog.parentNode.removeChild(dlg); }};
