22c34d8667cc5af20ff0381f46349213c7956748
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2018-11-26 18:14:15 -0500

    Parrot messages

---
+++ play/gossip/parrot.js
@@ -0,0 +1,63 @@
+if (!("parrot" in Tootsville))
+{ Tootsville.parrot = {}; }
+
+Tootsville.parrot.show = function (reallyp)
+{ var parrot = document.getElementById('parrot');
+  parrot.style.opacity = (reallyp ? 100 : 0);
+  document.getElementById('parrot-image').style.left = (reallyp ? 0 : '-500px');
+  if (reallyp)
+  { parrot.style.display = 'block';
+    parrot.style.zIndex = 2000; }
+  else
+  { setTimeout(() => { parrot.style.display = 'none'; }, 1000 ); }}
+
+Tootsville.parrot.done = function ()
+{ return new Promise
+  ( (finish) => { Tootsville.parrot.show (false); finish (); } ); }
+
+Tootsville.parrot.say = function (title, message)
+{ return new Promise
+  ( (finish) => 
+    { var reply = document.getElementById('parrot-reply');
+      reply.style.display = 'none';
+      reply.innerHTML = '';
+      var buttonBox = document.createElement('DIV');
+      buttonBox.setAttribute ('class', 'button-box');
+      var okButton = document.createElement('BUTTON');
+      okButton.innerText = ' O.K. ';
+      okButton.onclick = () => { Tootsville.parrot.done().then(() => {finish ()}); };
+      buttonBox.append (okButton);
+      reply.append (buttonBox);
+      reply.style.display = 'block';
+      var speech = document.getElementById('parrot-speech');
+      speech.style.display = 'none';
+      speech.innerHTML = '<h2>' + title + '</h2>' + message;
+      speech.style.display = 'block';
+      Tootsville.parrot.show(true); })}
+
+Tootsville.parrot.ask = function (title, message, replies)
+{ return new Promise
+  ( (finish) => 
+    { var reply = document.getElementById ('parrot-reply');
+      reply.style.display = 'none';
+      reply.innerHTML = '';
+      var buttonBox = document.createElement('DIV');
+      buttonBox.setAttribute ('class', 'button-box');
+      replies.forEach( (reply) =>
+      { var button = document.createElement ('BUTTON');
+        button.innerText = reply.text;
+        button.onclick = () => { Tootsville.parrot.done().then(function () {finish (reply.tag)}); };
+        buttonBox.append (button); });
+      reply.append (buttonBox);
+      reply.style.display = 'block';
+      var speech = document.getElementById('parrot-speech');
+      speech.style.display = 'none';
+      speech.innerHTML = '<h2>' + title + '</h2>' + message;
+      speech.style.display = 'block';
+      Tootsville.parrot.show(true); })}
+
+Tootsville.parrot.ynP = function (title, message)
+{ return Tootsville.parrot.ask (title, message,
+                                [  { tag: false, text: "No" },
+                                   { tag: true, text: "Yes" } ]); }
+
