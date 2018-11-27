8f580628d39d4f1739e84d1f297c9ca643c80b4f
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2018-11-26 21:59:40 -0500

    prepare for OpenYOLO conversion

--- play/login/login.js
+++ play/login/login.js
@@ -11,25 +11,6 @@ Tootsville.login.start = function ()
   console.log("Start login");
   Tootsville.ui.hud.loadHUDPanel("login", Tootsville.login.beginYolo); }
 
-Tootsville.login.beginYolo = function (loginPanel)
-{ Tootsville.ui.hud.loadScriptIntoDiv("https://smartlock.google.com/client", loginPanel); }
-
-// Tootsville.login.googleInit = function ()
-// { console.log("Async Google init");
-//   if (!("gapi" in window))
-//   { console.error ("No gapi in window");
-//     return; }
-//   gapi.load('auth2', () =>
-//             { gapi.auth2.init().then(
-//                 () =>
-//                     { console.log("Got Google login success response");
-//                       Tootsville.login.googleSignIn(); },
-//                 (err) =>
-//                     { console.log("Got Google login error response");
-//                       document.getElementById('sign-in').style.display = 'none';
-//                       document.getElementById('google-init-failed-reason').innerHTML = err;
-//                       document.getElementById('google-init-failed').style.display = 'block'; }); }); }
-
 Tootsville.login.storeGoogleCredentials = function ()
 { document.cookie = 'google-api-token=' + Tootsville.login.googleIDToken + ';path=/;domain=' +
   Tootsville.cluster + ';max-age=' + (365 * 24 * 60 * 60); }
@@ -44,12 +25,6 @@ Tootsville.login.googleSignOut = function ()
 
 Tootsville.login.switchGoogleAccount = function ()
 { console.log("Switch Google Account: TODO");
-  // if (window.gapi)
-  // { console.log("Using gapi");
-  //   Tootsville.login.googleSignOut().then(Tootsville.login.googleInit); } else if (window.googleyolo)
-  // { console.log("Using Yolo");
-  //   Tootsville.login.googleYoloSignIn(window.googleyolo); } else
-  // { console.error ("Neither gapi nor yolo found"); }
 }
 
 Tootsville.login.drawAvatarOnCanvas = function (avatar,canvas)
@@ -296,64 +271,4 @@ Tootsville.login.onSignIn = function (googleUser)
     Tootsville.login.switchTootsView(); } else
   { Tootsville.login.startSignIn(); } };
 
-Tootsville.login.gotYolo = function (credential)
-{ if (credential.idToken)
-  { console.log("Got credentials", credential);
-    Tootsville.login.googleIDToken = credential.idToken;
-    Tootsville.login.storeGoogleCredentials();
-    Tootsville.login.googleUser.getBasicProfile().then(
-    (profile) => 
-    { if (! Tootsville.login.player) { Tootsville.login.player = {} ; }
-      Tootsville.login.player.name = profile.getName();
-      Tootsville.login.player.email = profile.getEmail();
-      Tootsville.login.player.face = profile.getImageUrl(); }); }
-  else
-  { console.log("Mysterious credentials lack an idToken", credential); } };
-
-Tootsville.login.yoloFailed = function (error)
-{ Tootsville.error("Yolo Error", error);
-  switch (error.type) {
-  case "userCanceled":
-      /* The user closed the hint selector. */
-      Tootsville.login.googleYoloSignin();
-      break;
-  case "noCredentialsAvailable":
-      /* No hint available for the session. */
-      break;
-  case "requestFailed":
-      /* The request failed, most likely because of a timeout.
-         You can retry another time if necessary. */
-      Tootsville.login.googleYoloSignin();
-      break;
-  case "operationCanceled":
-      // The operation was programmatically canceled, do nothing.
-      break;
-  case "illegalConcurrentRequest":
-      // Another operation is pending, this one was aborted.
-      break;
-  case "initializationError":
-      // Failed to initialize. Refer to error.message for debugging.
-      break;
-  case "configurationError":
-      // Configuration error. Refer to error.message for debugging.
-      break;
-  default:
-      // Unknown error, do nothing.
-  }};
-
-Tootsville.login.googleYoloSignIn = function ()
-{ console.log("Asking Google YOLO to run");
-  googleyolo.retrieve(
-      { supportedAuthMethods:
-        [ "https://accounts.google.com" ],
-        supportedIdTokenProviders:
-        [{ uri: "https://accounts.google.com",
-           /* TODO move Google client ID someplace cooler */
-           clientId: "1052481910502-kb8u1evb6c82dl2o5km4vu6q5mmudkfo.apps.googleusercontent.com" }],
-        context: "signIn" /* or "continue" */ }).then(
-            (credential) => {Tootsville.login.gotYolo(credential);},
-            (error) => {Tootsville.login.yoloFailed(error);}); };
-
-Tootsville.login.cancelYoloSignIn = function ()
-{ googleyolo.cancelLastOperation (); }
 
--- play/login/yolo.js
+++ play/login/yolo.js
@@ -13,6 +13,11 @@ Tootsville.login.yoloConfig = {
     ]
 };
 
+Tootsville.login.beginYolo = function (loginPanel)
+{ Tootsville.ui.hud.loadScriptIntoDiv('',
+                                      loginPanel).then(); }
+
+
 Tootsville.login.yoloError = function (error) {
     Tootsville.inform('YOLO Error 2', error.type, error.message, error);
     switch (error.type) {
@@ -92,3 +97,64 @@ Tootsville.login.googleYoloSignIn = function (yolo) {
         }, Tootsville.login.yoloError);
     });
 };
+
+Tootsville.login.gotYolo = function (credential)
+{ if (credential.idToken)
+  { console.log("Got credentials", credential);
+    Tootsville.login.googleIDToken = credential.idToken;
+    Tootsville.login.storeGoogleCredentials();
+    Tootsville.login.googleUser.getBasicProfile().then(
+    (profile) => 
+    { if (! Tootsville.login.player) { Tootsville.login.player = {} ; }
+      Tootsville.login.player.name = profile.getName();
+      Tootsville.login.player.email = profile.getEmail();
+      Tootsville.login.player.face = profile.getImageUrl(); }); }
+  else
+  { console.log("Mysterious credentials lack an idToken", credential); } };
+
+Tootsville.login.yoloFailed = function (error)
+{ Tootsville.error("Yolo Error", error);
+  switch (error.type) {
+  case "userCanceled":
+      /* The user closed the hint selector. */
+      Tootsville.login.googleYoloSignin();
+      break;
+  case "noCredentialsAvailable":
+      /* No hint available for the session. */
+      break;
+  case "requestFailed":
+      /* The request failed, most likely because of a timeout.
+         You can retry another time if necessary. */
+      Tootsville.login.googleYoloSignin();
+      break;
+  case "operationCanceled":
+      // The operation was programmatically canceled, do nothing.
+      break;
+  case "illegalConcurrentRequest":
+      // Another operation is pending, this one was aborted.
+      break;
+  case "initializationError":
+      // Failed to initialize. Refer to error.message for debugging.
+      break;
+  case "configurationError":
+      // Configuration error. Refer to error.message for debugging.
+      break;
+  default:
+      // Unknown error, do nothing.
+  }};
+
+Tootsville.login.googleYoloSignIn = function ()
+{ console.log("Asking Google YOLO to run");
+  googleyolo.retrieve(
+      { supportedAuthMethods:
+        [ "https://accounts.google.com" ],
+        supportedIdTokenProviders:
+        [{ uri: "https://accounts.google.com",
+           /* TODO move Google client ID someplace cooler */
+           clientId: "1052481910502-kb8u1evb6c82dl2o5km4vu6q5mmudkfo.apps.googleusercontent.com" }],
+        context: "signIn" /* or "continue" */ }).then(
+            (credential) => {Tootsville.login.gotYolo(credential);},
+            (error) => {Tootsville.login.yoloFailed(error);}); };
+
+Tootsville.login.cancelYoloSignIn = function ()
+{ googleyolo.cancelLastOperation (); }
