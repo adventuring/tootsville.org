113ef37d7322af103782c4616667d83129644bf3
Bruce-Robert Pocock <brpocock@star-hope.org>
2018-12-10 16:32:10 -0500

    allow DEFPOST macro to be loaded first

--- servers/src/package.lisp
+++ servers/src/package.lisp
@@ -39,11 +39,3 @@
    #:write-docs
    ))
 
-(defpost ensure-package-imports-from-oliphaunt-are-available ()
-  (macro-function 'Tootsville::define-memo-function) ()
-  "Did not import symbols correctly; `DEFINE-MEMO-FUNCTION' should ~
- be a macro, but it is not.
-
-Got: ~a"
-  (with-output-to-string (*standard-output*)
-    (describe 'Tootsville::define-memo-function)))
