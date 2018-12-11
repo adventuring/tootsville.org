288eb9d85af949efb1ae85e86fe64bce76fe8732
Bruce-Robert Pocock <brpocock@star-hope.org>
2018-12-11 10:59:47 -0500

    add MAKE-RECORD, clean up DEFRECORD

--- servers/src/db/db-central.lisp
+++ servers/src/db/db-central.lisp
@@ -35,75 +35,106 @@
         (:uuid `(vector-to-int ,slot))       
         (:timestamp `(timestamp-to-universal  ,slot))))))
 
+(defun defrecord/load-record (name columns)
+  `(defmethod load-record ((class (eql ',name)) row)
+     (,(intern (concatenate 'string "MAKE-" (string name)))
+       ,@(loop for i from 0 
+            for column in columns
+            appending (column-load-mapping column i)))))
+
+(defun defrecord/find-record (name table)
+  `(defmethod find-record ((class (eql ',name)) &rest columns+values)
+     (load-record ',name (apply #'db-select-single-record 
+                                ,(lisp-to-db-name table)
+                                columns+values))))
+
+(defun defrecord/find-records (name table)
+  `(defmethod find-records ((class (eql ',name)) &rest columns+values)
+     (mapcar #'load-record ',name
+	   (apply #'db-select-records-simply ,(lisp-to-db-name table)
+		columns+values))))
+
+(defun defrecord/row<-record (name columns)
+  `(defmethod row<-record  ((object ,name))
+     (with-slots ,(mapcar #'car columns) object
+       (list ,@(mapcar #'column-save-mapping columns)))))
+
+(defun defrecord/save-record (name id-accessor database table columns )
+  `(defmethod save-record ((object ,name))
+     (with-dbi (,database)
+       (if (null (,id-accessor object))
+           (progn 
+             (when (string-equal (caar columns) "UUID")
+               (setf (,id-accessor object) (uuid:make-v4-uuid)))
+             (let* ((query (cl-dbi:prepare 
+                            *dbi-connection*
+                            ,(format nil "INSERT INTO `~a` (~{`~a`~^, ~})~
+~:*VALUES (~{?~*~^, ~})"
+                                     table
+                                     (mapcar (compose #'lisp-to-db-name #'car) columns)))))
+               (cl-dbi:execute query
+                               ,@(mapcar #'column-save-mapping columns)))
+             (when (string-equal (caar columns) "ID")
+               (let* ((id-query (cl-dbi:prepare 
+                                 *dbi-connection*
+                                 "SELECT LAST_INSERT_ID()"))
+                      (id-results (cl-dbi:execute id-query)))
+                 (setf (,id-accessor object) (caar (cl-dbi:fetch-all 
+                                                    id-results))))))
+           (let ((query (cl-dbi:prepare
+                         *dbi-connection*
+                         ,(format nil "UPDATE `~a` ~
+SET ~{`~a` = ?~^, ~} WHERE ~a = ?"
+                                  table
+                                  (mapcar (compose #'lisp-to-db-name #'car)
+                                          (rest columns))
+                                  (lisp-to-db-name (caar columns))))))
+             (cl-dbi:execute query
+                             ,@(mapcar #'column-save-mapping (rest columns))
+                             ,(caar columns)))))))
+
+(defun defrecord/save-record-with-id-column (name database table columns)
+  (when (or (string-equal "ID" (caar columns))
+            (string-equal "UUID" (caar columns)))
+    (let ((id-accessor (intern (concatenate 'string (string name) "-" 
+                                            (string (caar columns))))))
+      `(progn
+         ,(defrecord/save-record name id-accessor database table columns )))))
+
+(defun defrecord/find-reference (name column)
+  `(defmethod find-reference
+       ((object ,name)
+        (reference (eql ,(make-keyword (string (first column))))))
+     (find-record ',(fourth column)
+	        ,(lisp-to-db-name (first column)) 
+                  (,(intern (concatenate 'string (string name)
+                                         "-"
+                                         (string (first column))))
+                    object))))
+
+(defun defrecord/find-reference-columns (name columns)
+  (when (find-if (lambda (column) (< 2 (length column))) columns)
+    (cons 'progn
+          (loop for column in columns
+	   when (and (= 4 (length column))
+		   (string-equal "REF" (third column)))
+	   collecting
+	     (defrecord/find-reference name column)))))
+
+(defun defrecord/make-record (name columns)
+  `(defmethod make-record ((class (eql ',name)) &rest columns+values)
+     (let ((record (apply #'make-instance ',name columns+values)))
+       (save-record record)
+       record)))
+
 (defmacro defrecord (name (database table) &rest columns)
   `(progn 
      (defstruct ,name
        ,@(mapcar #'car columns))
-     (defmethod load-record ((class (eql ',name)) row)
-       (,(intern (concatenate 'string "MAKE-" (string name)))
-         ,@(loop for i from 0 
-              for column in columns
-              appending (column-load-mapping column i))))
-     (defmethod find-record ((class (eql ',name)) &rest columns+values)
-       (load-record ',name (apply #'db-select-single-record 
-                                  ,(lisp-to-db-name table)
-                                  columns+values)))
-     (defmethod find-records ((class (eql ',name)) &rest columns+values)
-       (mapcar #'load-record ',name
-	     (apply #'db-select-records-simply ,(lisp-to-db-name table)
-		  columns+values)))
-     (defmethod row<-record  ((object ,name))
-       (with-slots ,(mapcar #'car columns) object
-         (list ,@(mapcar #'column-save-mapping columns))))
-     ,(when (or (string-equal "ID" (caar columns))
-                (string-equal "UUID" (caar columns)))
-        (let ((id-accessor (intern (concatenate 'string (string name) "-" 
-                                                (string (caar columns))))))
-          `(progn
-	   (defmethod save-record ((object ,name))
-               (with-dbi (,database)
-                 (if (null (,id-accessor object))
-                     (progn 
-                       (when (string-equal (caar columns) "UUID")
-                         (setf (,id-accessor object) (uuid:make-v4-uuid)))
-                       (let* ((query (cl-dbi:prepare 
-                                      *dbi-connection*
-                                      ,(format nil "INSERT INTO `~a` (~{`~a`~^, ~})~
-~:*VALUES (~{?~*~^, ~})"
-                                               table
-                                               (mapcar (compose #'lisp-to-db-name #'car) columns)))))
-                         (cl-dbi:execute query
-                                         ,@(mapcar #'column-save-mapping columns)))
-                       (when (string-equal (caar columns) "ID")
-                         (let* ((id-query (cl-dbi:prepare 
-                                           *dbi-connection*
-                                           "SELECT LAST_INSERT_ID()"))
-                                (id-results (cl-dbi:execute id-query)))
-                           (setf (,id-accessor object) (caar (cl-dbi:fetch-all 
-                                                              id-results))))))
-                     (let ((query (cl-dbi:prepare
-                                   *dbi-connection*
-                                   ,(format nil "UPDATE `~a` ~
-SET ~{`~a` = ?~^, ~} WHERE ~a = ?"
-                                            table
-                                            (mapcar (compose #'lisp-to-db-name #'car)
-                                                    (rest columns))
-                                            (lisp-to-db-name (caar columns))))))
-                       (cl-dbi:execute query
-                                       ,@(mapcar #'column-save-mapping (rest columns))
-                                       ,(caar columns)))))))))
-     ,(when (find-if (lambda (column) (< 2 (length column))) columns)
-        (cons 'progn
-              (loop for column in columns
-	       when (and (= 4 (length column))
-		       (string-equal "REF" (third column)))
-	       collecting
-	         `(defmethod find-reference
-	              ((object ,name)
-		     (reference (eql ,(make-keyword (string (first column))))))
-	            (find-record ',(fourth column)
-			     ,(lisp-to-db-name (first column)) 
-                                   (,(intern (concatenate 'string (string name)
-                                                          "-"
-                                                          (string (first column))))
-                                     object))))))))
+     ,(defrecord/make-record name columns)
+     ,(defrecord/load-record name columns)
+     ,(defrecord/find-record name table)
+     ,(defrecord/find-records name table)
+     ,(defrecord/row<-record name columns)
+     ,(defrecord/save-record-with-id-column name database table columns)
+     ,(defrecord/find-reference-columns name columns)))
