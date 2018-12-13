2ca60f9b5169d9218f331c0e47bfbb5ff33a36e2
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2018-12-12 21:19:43 -0500

    obnoxious parrot squawking

--- play/gossip/parrot.js
+++ play/gossip/parrot.js
@@ -7,7 +7,9 @@ Tootsville.parrot.show = function (reallyp)
   document.getElementById('parrot-image').style.left = (reallyp ? 0 : '-500px');
   if (reallyp)
   { parrot.style.display = 'block';
-    parrot.style.zIndex = 2000; }
+    parrot.style.zIndex = 2000;
+    if (Tootsville.parrot.squawk)
+    {Tootsville.parrot.squawk.play (); } }
   else
   { setTimeout(() => { parrot.style.display = 'none'; }, 1000 ); }}
 
