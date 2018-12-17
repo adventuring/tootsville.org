3532eeb1b4e364b517f5e3274c068c18c9989f0c
Bruce-Robert Pocock <brpocock@star-hope.org>
2018-12-16 19:00:39 -0500

    more careful check for Rollbar

--- play/analytics.js
+++ play/analytics.js
@@ -5,14 +5,14 @@ Tootsville.inform = function (event, details) {
     console.info.apply(console, arguments);
     dataLayer.push({event: event, details: arguments});
     ga('send', arguments[0], Array.join(arguments.splice(1), "\n"));
-    if (Rollbar && Rollbar.info)
+    if (('Rollbar' in window) && Rollbar.info)
     { Rollbar.info(Array.prototype.splice.call(arguments).map((x) => {x.toSource()}).join('\t')); }
 };
 Tootsville.warn = function (message) {
     console.warn.apply(console, arguments);
     dataLayer.push({event: 'warning', details: arguments});
     ga('send', 'warning', Array.join(arguments, "\n"));
-    if (Rollbar && Rollbar.info)
+    if (('Rollbar' in window) && Rollbar.warn)
     { Rollbar.warn.apply(Rollbar, Array.prototype.splice.call(arguments).map((x) => {x.toSource()}).join('\t')); }
 };
 Tootsville.error = function (message) {
