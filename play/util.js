628e3c68b1ce30b4b7d33d650db3523348fad2bd
Bruce-Robert Pocock <brpocock@star-hope.org>
2018-12-16 19:01:04 -0500

    Enhance error handling for REST calls

--- play/util.js
+++ play/util.js
@@ -16,10 +16,27 @@ Tootsville.util.assertValidHostName = function (hostName)
   return undefined;
 };
 
+Tootsville.parrot.parrotErrorText = function (body)
+{ let code = body.error;
+  let text = body.status;
+  if (! text)
+  { if (code)
+    { text = "HTTP Error code " + code; }
+    else
+    { text = "The server did not respond with a regonizeable error reply." } }
+  if (! code)
+  { code = 500; }
+  return "<P>The server reported an error.</P>" +
+  "<P>" + text + "</P>" +
+  "<P> <A TARGET=\"new\"" +
+  " HREF=\"https://wiki.tootsville.org/wiki/Error_" + code + "\">Learn more ...</A></P>"; }
+    
+
 Tootsville.util.rest = function (method, uri, body, headers)
 { let hostName = uri.split('/')[0];
-  hostName = Tootsville.util.assertValidHostName(hostName);
-  uri = hostName + '/' + uri;
+  if (!(hostName == "http"))
+  { hostName = Tootsville.util.assertValidHostName(hostName);
+    uri = hostName + '/' + uri; }
   Tootsville.trace ('REST: ' + method + ' ' + uri);
   if (!headers) { headers = {}; }
   if (! ('Accept' in headers))
@@ -31,7 +48,26 @@ Tootsville.util.rest = function (method, uri, body, headers)
   { headers['Content-Type'] = 'application/json';
     opts.body = body; }
   opts.headers = headers;
-  return fetch (uri, opts).then( response => response.json () ); };
+  return fetch (uri, opts).then(
+      response =>
+          { if (response.ok)
+            { return response.json (); }
+            else
+            { Tootsville.warn("Server error " + JSON.stringify(response.json ()));
+              Tootsville.parrot.ask (
+                  "Uh-oh! Server trouble!",
+                  Tootsville.parrot.parrotErrorText(response.json ()),
+                  [{ retry: "Retry the network operation" }]).then
+              (() =>
+               { return Tootsville.util.rest (method, uri, body, headers); }); }},
+      error =>
+          { Tootsville.warn("Fetch error " + error);
+            Tootsville.parrot.ask (
+                "Uh-oh! Network trouble!",
+                "I got a network error: " + error + "<BR><BR>Did we get disconnected?",
+                [{ retry: "Retry the network operation" }]).then
+            (() =>
+             { return Tootsville.util.rest (method, uri, body, headers); });} ); };
 
 Tootsville.util.loadScript = function (src)
 { return new Promise( finish =>
