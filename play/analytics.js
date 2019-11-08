a0feb5bdee617852f4cdda319209f04c9ef2e6b8
Bruce-Robert Pocock <brpocock@star-hope.org>
2019-11-08 10:24:41 -0500

    fix Rollbar calls

--- play/analytics.js
+++ play/analytics.js
@@ -31,29 +31,25 @@
  *
  */
 
-Tootsville.trace = function (event, details) {
-    console.log.apply(console, arguments);
-};
-Tootsville.inform = function (event, details) {
-    console.info.apply(console, arguments);
-    dataLayer.push({event: event, details: arguments});
-    // ga('send', arguments[0], Array.prototype.join(arguments.splice(1), "\n"));
-    if (('Rollbar' in window) && Rollbar.info)
-    { Rollbar.info(Array.prototype.splice.call(arguments).
-                   map((x) => {x.toSource();}).join('\t')); }
-};
-Tootsville.warn = function (message) {
-    console.warn.apply(console, arguments);
-    dataLayer.push({event: 'warning', details: arguments});
-    ga('send', 'warning', Array.prototype.join(arguments, "\n"));
-    if (('Rollbar' in window) && Rollbar.warn)
-    { Rollbar.warn.apply (Rollbar,
-                          Array.prototype.splice.call(arguments).
-                          map((x) => {x.toSource();}).join('\t')); }
-};
-Tootsville.error = function (message) {
-    console.error.apply(console, arguments);
-    dataLayer.push({event: 'error', details: arguments});
-    ga('send', 'error', Array.prototype.join(arguments, "\n"));
-    throw new Error(message);
-};
+Tootsville.trace = function (event, details)
+{ console.log.apply(console, arguments); };
+
+Tootsville.inform = function (event, details)
+{ console.info.apply(console, arguments);
+  dataLayer.push({event: event, details: arguments});
+  // ga('send', arguments[0], Array.prototype.join(arguments.splice(1), "\n"));
+  if (('Rollbar' in window) && Rollbar.info)
+  { Rollbar.info (arguments.toSource ()); } };
+
+Tootsville.warn = function (message)
+{ console.warn.apply(console, arguments);
+  dataLayer.push({event: 'warning', details: arguments});
+  ga('send', 'warning', Array.prototype.join(arguments, "\n"));
+  if (('Rollbar' in window) && Rollbar.warn)
+  { Rollbar.warn (arguments.toSource ()); } };
+
+Tootsville.error = function (message)
+{ console.error.apply(console, arguments);
+  dataLayer.push({event: 'error', details: arguments});
+  ga('send', 'error', Array.prototype.join(arguments, "\n"));
+  throw new Error(message); };
