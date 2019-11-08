26d4593349df607902dc870f5674e97eefed2767
Bruce-Robert Pocock <brpocock@star-hope.org>
2019-11-08 10:32:45 -0500

    use console.info for User-visible notes

--- play/game/start.js
+++ play/game/start.js
@@ -40,7 +40,7 @@ window.addEventListener ('load', () => {
         document.getElementById ('control-panel-icon').style.width = '7vw';
     }, 1000);
     
-    console.log ("\n\n\n This version of Tootsville is Build ID: @@BUILD@@\n ——————————————————————————————————————————————————————\n\n");
+    console.info ("\n\n\n This version of Tootsville is Build ID: @@BUILD@@\n ——————————————————————————————————————————————————————\n\n");
 
     window.addEventListener ('click', Tootsville.ui.onFirstClick);
     document.addEventListener ('fullscreenchange',
