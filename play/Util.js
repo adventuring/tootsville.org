66d96acba8e9f728d0048c5221d8a7bc66e9535d
Bruce-Robert Pocock <brpocock@star-hope.org>
2020-10-24 00:05:04 -0400

    tidy formatting

--- play/Util.js
+++ play/Util.js
@@ -145,28 +145,27 @@ Tootsville.Util.ensureServersReachable = function ()
 /**
  * Check for value equality of two objects
  */
-Tootsville.Util.equalP = function (x, y) {
-    'use strict';
+Tootsville.Util.equalP = function (x, y)
+{ 'use strict';
 
-    if (x === null || x === undefined || y === null || y === undefined) { return x === y; }
-    // after this just checking type of one would be enough
-    if (x.constructor !== y.constructor) { return false; }
-    // if they are functions, they should exactly refer to same one (because of closures)
-    if (x instanceof Function) { return x === y; }
-    // if they are regexps, they should exactly refer to same one (it is hard to better equality check on current ES)
-    if (x instanceof RegExp) { return x === y; }
-    if (x === y || x.valueOf() === y.valueOf()) { return true; }
-    if (Array.isArray(x) && x.length !== y.length) { return false; }
+  if (x === null || x === undefined || y === null || y === undefined) { return x === y; }
+  // after this just checking type of one would be enough
+  if (x.constructor !== y.constructor) { return false; }
+  // if they are functions, they should exactly refer to same one (because of closures)
+  if (x instanceof Function) { return x === y; }
+  // if they are regexps, they should exactly refer to same one (it is hard to better equality check on current ES)
+  if (x instanceof RegExp) { return x === y; }
+  if (x === y || x.valueOf() === y.valueOf()) { return true; }
+  if (Array.isArray(x) && x.length !== y.length) { return false; }
 
-    // if they are dates, they must had equal valueOf
-    if (x instanceof Date) { return false; }
+  // if they are dates, they must had equal valueOf
+  if (x instanceof Date) { return false; }
 
-    // if they are strictly equal, they both need to be object at least
-    if (!(x instanceof Object)) { return false; }
-    if (!(y instanceof Object)) { return false; }
+  // if they are strictly equal, they both need to be object at least
+  if (!(x instanceof Object)) { return false; }
+  if (!(y instanceof Object)) { return false; }
 
-    // recursive object equality check
-    let p = Object.keys(x);
-    return Object.keys(y).every(function (i) { return p.indexOf(i) !== -1; }) &&
-        p.every(function (i) { return Tootsville.Util.equalP (x[i], y[i]); });
-};
+  // recursive object equality check
+  let p = Object.keys(x);
+  return Object.keys(y).every(function (i) { return p.indexOf(i) !== -1; }) &&
+  p.every(function (i) { return Tootsville.Util.equalP (x[i], y[i]); }); };
