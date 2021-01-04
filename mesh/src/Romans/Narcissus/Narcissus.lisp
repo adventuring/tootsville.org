c53b49e378d702363782ab4cfebdeec7d4215782
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-01-03 21:39:34 -0500

    © 2021 update

--- mesh/src/Romans/Narcissus/Narcissus.lisp
+++ mesh/src/Romans/Narcissus/Narcissus.lisp
@@ -3,7 +3,7 @@
 ;;;; ./mesh/src/Romans/Narcissus/Narcissus.lisp is part of Tootsville
 ;;;
 ;;;; Copyright  ©  2006–2010,  2012, 2014–2017  Bruce-Robert  Pocock;  ©
-;;;; 2018-2020 The  Corporation for Inter-World Tourism  and Adventuring
+;;;; 2018-2021 The  Corporation for Inter-World Tourism  and Adventuring
 ;;;; (ciwta.org).
 ;;;
 ;;;; This  program is  Free  Software: you  can  redistribute it  and/or
