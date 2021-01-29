e7257fdb2fdd3ba7657d40e1729593e87ce0f54e
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-01-29 15:53:15 -0500

    convert all Blender files utility script

---
+++ bin/convert-all-blender-files.py
@@ -0,0 +1,19 @@
+import bpy
+import os
+
+path = '/home/brpocock/Projects/jumbo.tootsville.org/'  # set this path
+
+for root, dirs, files in os.walk(path):
+    for f in files:
+        if f.endswith('.obj') :
+            obj_file = os.path.join(path, f)
+            glb_file = os.path.splitext(mesh_file)[0] + ".glb"
+
+            bpy.ops.object.select_all(action='SELECT')
+            bpy.ops.object.delete()
+
+            bpy.ops.import_scene.obj(filepath=obj_file)
+
+            bpy.ops.object.select_all(action='SELECT')
+
+            bpy.ops.export_scene.gltf(filepath=glb_file,export_copyright="© 2021 CIWTA.org")
