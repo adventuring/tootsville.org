41d084d2fd502eba2276f738d2edd3f1ec185b00
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-02-07 15:39:54 -0500

    speling

--- play/Login/Login.js
+++ play/Login/Login.js
@@ -468,7 +468,7 @@ Tootsville.Login.firebaseLogin = function (loginPanel)
   yahoo.setCustomParameters({ prompt: 'login' });
   document.getElementById ('login-13').innerHTML += `
 <center>
-  <button id="firebase-sign-in-with-yahoo">SIgn in with Yahoo!</button>
+  <button id="firebase-sign-in-with-yahoo">Sign in with Yahoo!</button>
 </center>`;
   document.getElementById ('firebase-sign-in-with-yahoo').addEventListener (
       'click',
