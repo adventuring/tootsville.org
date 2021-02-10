41f5db9fa7ecbef309cedbfd2fc4eab859c899d4
Bruce-Robert Pocock <brpocock@star-hope.org>
2021-02-10 15:49:35 -0500

    towards specialTexture handling

--- play/Scene/FurnitureBuilder.js
+++ play/Scene/FurnitureBuilder.js
@@ -157,17 +157,146 @@ Tootsville.FurnitureBuilder.build = function (item, scene=undefined, finish=unde
 /*----------------------------------------*/
 
 /**
+ * Set ``material'' to a pixmap (PNG or JPEG) image
+ */
+Tootsville.FurnitureBuilder.setMaterialPixmapTexture = function (material, texture) {
+    // UNIMPLEMENTED special texture pixmap
+};
+
+/**
+ * Set ``material'' to an SVG image
+ */
+Tootsville.FurnitureBuilder.setMaterialVectorTexture = function (material, texture) {
+     // UNIMPLEMENTED special texture SVG
+};
+
+/**
+ * Set ``material'' to a video texture.
+ *
+ * Three files will be used:
+ *
+ * @itemize
+ * @item
+ * The base filename ending in @code{mp4},
+ * @item
+ * The base filename ending in @code{ogv},
+ * @item
+ * and a ``loading poster'' image with the base filename 
+ * ending in @code{jpg}.
+ * @end itemize
+ */
+Tootsville.FurnitureBuilder.setMaterialVideoTexture = function (material, texture) {
+    // UNIMPLEMENTED special texture video
+};
+
+/**
+ * UNIMPLEMENTED special handling for Theater in Toot Square West screen
+ */
+Tootsville.FurnitureBuilder.theaterWestVideoTexture = function (material) {
+    // UNIMPLEMENTED Theater West video texture
+};
+
+
+/**
+ * Items may have a material texture applied on a per-item basis. The
+ * material must be named (case-insensitive) @code{map} and the texture
+ * filename is given by the item's @code{specialTexture} field.
+ *
+ * @subsection Kinds of Special Textures
+ *
+ * Ordinarily, this will be a PNG or JPEG file located at
+ * @code{https://jumbo.tootsville.org/Assets/Textures/5/} whose name
+ * is given in @code{specialTexture} ending in @code{.png}
+ * or @code{.jpg} or @code{.jpeg}.
+ *
+ * However, the special texture may also be an SVG image which will be
+ * drawn onto a texture (probably at 1024×1024 px), if its name ends
+ * in @code{.svg}.
+ *
+ * The special texture may also be a video stream, which will be
+ * played on a silent loop, when its name ends in @code{.mp4}
+ * or @code{.ogv}.  Note that we will attempt to load @emph{both} the
+ * MPEG 4 and OGG Vorbis forms of the video simultaneously in
+ * different browsers, so it is not defined which of the two will be
+ * loaded by any particular user (due to CODEC issues in browsers).  A
+ * third file, in @code{JPEG} format and named ending in @code{.jpg}
+ * will be used as the ``poster'' image texture until the video has
+ * loaded.
+ *
+ * @subsection Very Special Special Textures
+ *
+ * Other special cases can be specified with names that begin with the
+ * character @code{#}, since that is forbidden in URLs (and used in
+ * the operator command language in Tootsville as well). Currently,
+ * only one such extension is defined, which is @code{#theater-west}.
+ * 
+ * @subsubsection #theater-west
+ *
+ * The special code @code{#theater-west} is used only for the movie
+ * screen in the theater located in Toot Square West, which has some
+ * special behaviors.
+ *
+ * First, the theater's current film is selected by a server-side
+ * setting from the @code{Assets/Movies/5/} path.
+ *
+ * Second, the film plays at the same time offset for all users,
+ * modulo the running length of the film.
+ *
+ * Third, the film plays with sound enabled.
+ */
+Tootsville.FurnitureBuilder.setMaterialTexture = function (material, texture) {
+    if (/\.png$/.test(texture))
+        Tootsville.FurnitureBuilder.setMaterialPixmapTexture (material, texture);
+    else if (/\.jpe?g$/.test(texture))
+        Tootsville.FurnitureBuilder.setMaterialPixmapTexture (material, texture);
+    else if (/\.svg$/.test(texture))
+        Tootsville.FurnitureBuilder.setMaterialVectorTexture (material, texture);
+    else if (/\.(ogv|mp4)$/.test(texture))
+        Tootsville.FurnitureBuilder.setMaterialVideoTexture (material, texture);
+    else if ('#theater-west' === texture)
+        Tootsville.FurnitureBuilder.theaterWestVideoTexture (material);
+    else
+        console.warn ("Unsupported specialTexture value:", texture); };
+
+/**
  * Make a colorizer function for a material for ``furniture''.
+ *
+ * Materials in an item's model can be recolored on a per-item basis
+ * when they have specific (case-insensitive) names for those
+ * materials.
+ *
+ * Color names are specific names understood by `PARSE-COLOR24' or the
+ * HTML #RGB or #RRGGBB forms. Other formats understood by
+ * `PARSE-COLOR24' are not understood by
+ * `Tootsville.UI.interpretTootColor' and are not supported. The game
+ * server will only send colors in these formats.
+ *
+ * Any material named @code{base} will have its diffuse color set to
+ * the @code{baseColor} of the item, with its specular color set to a
+ * lighter version of that same color.
+ *
+ * Any material named @code{alt} will likewise have its diffuse color
+ * set to the @code{altColor} of that item, with its specular color
+ * set to a lighter version of that same color.
+ *
+ * Finally, any material named @code{map} will have the diffuse color
+ * set to a texture map based on the file named
+ * in @code{specialTexture} of that item, if any. The specific file
+ * will be loaded by `Tootsville.FurnitureBuilder.setMaterialTexture'
+ * from a file whose path
+ * is @code{https://jumbo.tootsville.org/Assets/Textures/5/} and whose
+ * name on that path is given by @code{specialTexture}. It can be a
+ * PNG, JPEG, or SVG file. See @code{setMaterialTexture} for details.
  */
 Tootsville.FurnitureBuilder.makeFurnitureColorizeMaterial = function (furniture) {
     return function (material) {
         if (!(material.name)) return;
-        if ('base' == material.name.toLower ())
+        if ('base' == material.name.toLower () && furniture.baseColor)
             Tootsville.ModelLoader.setMaterialColor (material, furniture.baseColor);
-        else if ('alt' == material.name.toLower ())
+        else if ('alt' == material.name.toLower () && furniture.altColor)
             Tootsville.ModelLoader.setMaterialColor (material, furniture.altColor);
-        else if ('map' == material.name.toLower ())
-            /* FIXME texturize */
+        else if ('map' == material.name.toLower () && furniture.specialTexture)
+            Tootsville.FurnitureBuilder.setMaterialTexture (material, furniture.specialTexture);
             return;
     }; };
 
